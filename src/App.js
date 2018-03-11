7ebf200c87e41aa0fa42727189b4875d805d75f2
Jude Kuti <judekuti@gmail.com>
2018-03-11 07:43:38 -0500

    Make props and state functionality

--- src/App.js
+++ src/App.js
@@ -7,6 +7,19 @@ const API_Key = "5e1f89fe4ed775b4"
 
 
 class App extends React.Component {
+    state = {
+        temperature: undefined,
+        feels_like: undefined,
+        City: undefined,
+        State: undefined,
+        Country: undefined,
+        local_time: undefined,
+        wind: undefined,
+        icon: undefined,
+        icon_url: undefined,
+        error: undefined
+
+    }
 
     getWeather = async (e) =>{
         // Prevent full page refresh
@@ -16,13 +29,36 @@ class App extends React.Component {
         const api_call = await fetch(`http://api.wunderground.com/api/${API_Key}/conditions/q/${state}/${city}.json `);
         const data = await api_call.json();
         console.log(data);
+        this.setState({
+            temperature: data.current_observation.temp_f,
+            feels_like: data.current_observation.feelslike_f,
+            State: data.current_observation.display_location.state_name,
+            City: data.current_observation.display_location.city,
+            Country: data.current_observation..display_location.country,
+            local_time: data.current_observation.local_time_rfc822,
+            wind: data.current_observation.wind_string,
+            icon: data.current_observation.icon,
+            icon_url: data.current_observation.icon_url,
+            erroe: ""
+
+        })
     }
     render(){
         return (
             <div>
                 <Titles />
                 <Form getWeather={this.getWeather}/>
-                <Weather />
+                <Weather temperature={this.state.temperature}
+                        feels_like={this.state.feels_like}
+                        State={this.state.State}
+                        City={this.state.City}
+                        Country={this.state.Country}
+                        local_time={this.state.local_time}
+                        wind={this.state.wind}
+                        icon={this.state.icon}
+                        icon_url={this.state.icon_url}
+                        error: {this.state.error}
+                        />
 
             </div>
             );
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,10 +3,9 @@ import React from "react";
 class Weather extends React.Component {
     render() {
         return (
-            <div>
-            <h1> Weather Component</h1>
-            <p> Find out temperature conditions and more...</p>
-            </div>
+
+
+
             );
     }
 };
