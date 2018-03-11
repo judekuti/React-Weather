b84fdd641ba44486b7c4523a87dfe325fd4b6852
Jude Kuti <judekuti@gmail.com>
2018-03-11 06:08:46 -0500

    Create Form and Weather components

---
+++ src/components/Form.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Form extends React.Component {
+    render() {
+        return (
+            <div>
+            <h1> Form Component</h1>
+            <p> Find out temperature conditions and more...</p>
+            </div>
+            );
+    }
+};
+
+export default Form
+
---
+++ src/components/Weather.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Weather extends React.Component {
+    render() {
+        return (
+            <div>
+            <h1> Weather Component</h1>
+            <p> Find out temperature conditions and more...</p>
+            </div>
+            );
+    }
+};
+
+export default Weather
+
