30fc74f148cb0e2ee4ca87d8b18553c9859aa92b
Jude Kuti <judekuti@gmail.com>
2018-03-11 06:03:23 -0500

    Initial Commit

---
+++ src/components/Layout.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Layout extends React.Component {
+    render() {
+        return (
+            <div>
+            <h1> Another Weather Finder</h1>
+            <p> Find out temperature conditions and more...</p>
+            </div>
+            );
+    }
+};
+
+export default Layout
