53e541b65abefabfe83a1c2e9ff497f5ab2f1299
Jude Kuti <judekuti@gmail.com>
2018-03-11 06:23:21 -0500

    Make API call to weather-undeground

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import Titles from './components/Titles';
 import Form from './components/Form';
 import Weather from './components/Weather';
 
+const API_Key = "5e1f89fe4ed775b4"
+
+
 class App extends React.Component {
+
+    getWeather = async () =>{
+        const api_call = await fetch(`http://api.wunderground.com/api/${API_Key}/conditions/q/TX/Dallas.json `)
+    }
     render(){
         return (
             <div>
